04026bc613d3055074d68db2c47f89b5aac69416
Nicholas Dwinata <nicholas.dwinata@ti.ukdw.ac.id>
2025-07-22 07:27:45 +0700

    pp

--- routes/galeriRoutes.js
+++ routes/galeriRoutes.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const { getAllGaleri, uploadGaleri } = require('../controllers/galeriController');
+const multer = require("multer");
+const {
+	getAllGaleri,
+	uploadGaleri,
+} = require("../controllers/galeriController");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+	destination: (req, file, cb) => cb(null, "uploads/"),
+	filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 });
 
 const upload = multer({ storage });
 
-router.get('/', getAllGaleri);
-router.post('/', upload.single('image'), uploadGaleri);
+router.get("/", getAllGaleri);
+router.post("/", upload.single("image"), uploadGaleri);
 
 module.exports = router;
