e20a3665e75388cb464a558f2e2fd048e1200631
Nicholas Dwinata <nicholas.dwinata@ti.ukdw.ac.id>
2025-07-22 09:06:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const agendaRoutes = require("./routes/agendaRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+	cors({
+		origin: ["https://webjurug-frontend.vercel.app"],
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
